b15968aa5e62621253f24bea45169aeda2e3cc9c
Riley Flynn <riley@rileyflynn.me>
2017-02-15 14:58:44 -0330

    Added Pushover notification dispatcher.

--- Start.py
+++ Start.py
@@ -3,6 +3,7 @@ import os
 
 from SchoolTracker import SchoolTracker
 from SchoolTracker.Dispatcher.TwitterDispatcher import TwitterDispatcher
+from SchoolTracker.Dispatcher.PushoverDispatcher import PushoverDispatcher
 
 if os.getenv("SCHOOLTRACKER_DEBUG", False):
     log_level = logging.DEBUG
@@ -10,4 +11,5 @@ else:
     log_level = logging.INFO
 schooltracker = SchoolTracker(log_level)
 schooltracker.DispatcherManager.register_dispatcher(TwitterDispatcher)
+schooltracker.DispatcherManager.register_dispatcher(PushoverDispatcher)
 schooltracker.start()
